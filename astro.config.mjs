49953f4f3cd480acb601c944bd013b707378e7d2
omercohen <omeronce1994@gmail.com>
2025-07-22 03:07:50 +0300

    F*** github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://omeronce1995.github.io',
-    base: '/affair-coin-website/'
-})
+    base: '/affair-coin-website/',
+});
